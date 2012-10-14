239a532cd3e659bdce3552834b2d81f443194a0b
Oneplus <oneplus.lau@gmail.com>
2012-10-14 21:29:31 +0800

    add image

--- k-means/kmeans.py
+++ k-means/kmeans.py
@@ -6,15 +6,13 @@ from numpy import linalg, random, array, concatenate
 import matplotlib.pyplot as plot
 
 def sample(N):
-    X = concatenate([random.normal(-1.5, 1., N/2),
-        random.normal(1.5, 1., N/2)])
-    Y = concatenate([random.normal(-1.5, 1., N/2),
-        random.normal(1.5, 1., N/2)])
-
-    return array([X, Y]).reshape(N, 2)
+    return (array([random.normal(-1.5,1.,N/2),random.normal(-1.5,1.,N/2)]).reshape(N/2, 2),
+            array([random.normal(1.5,1.,N/2),random.normal(1.5,1.,N/2)]).reshape(N/2, 2))
 
 def vertical(p1, p2, box):
     assert (box[0]<box[2] and box[1]<box[3])
+
+    print p1, p2
     def inbox(p):
         return (p[0]>=box[0] and 
                 p[0]<=box[2] and 
@@ -24,21 +22,25 @@ def vertical(p1, p2, box):
     k = (p2[1]-p1[1])/(p2[0]-p1[0])
 
     x0=box[0]; y0=-1./k*(x0-(p1[0]+p2[0])/2.)+((p1[1]+p2[1])/2.)
-    x1=box[2]; y1=-1./k*(x0-(p1[0]+p2[0])/2.)+((p1[1]+p2[1])/2.)
+    x1=box[2]; y1=-1./k*(x1-(p1[0]+p2[0])/2.)+((p1[1]+p2[1])/2.)
     y2=box[1]; x2=-k*(y2-(p1[1]+p2[1])/2.)+((p1[0]+p2[0])/2.)
     y3=box[3]; x3=-k*(y3-(p1[1]+p2[1])/2.)+((p1[0]+p2[0])/2.)
 
-    #print [(x0, y0), (x1, y1), (x2, y2), (x3, y3)] 
     return [p for p in [(x0, y0), (x1, y1), (x2, y2), (x3, y3)] if inbox(p)]
 
 if __name__=="__main__":
     N = 1000
-    X = sample(N)
+    X1, X2 = sample(N)
+    X = concatenate([X1,X2])
 
-    print X
-    plot.plot([X[i,0] for i in xrange(N)],
-            [X[i,1] for i in xrange(N)],
-            'r.')
+    plot.plot([X1[i,0] for i in xrange(N/2)],
+            [X1[i,1] for i in xrange(N/2)],
+            'rx',
+            [X2[i,0] for i in xrange(N/2)],
+            [X2[i,1] for i in xrange(N/2)],
+            'b+')
+
+    plot.show()
 
     T = 10
     u1 = [array([random.rand()*2-2, random.rand()*2])]
@@ -71,16 +73,19 @@ if __name__=="__main__":
 
         plot.plot([b1[0], b2[0]],
                 [b1[1], b2[1]],
-                'b-',
-                [X[i,0] for i in xrange(N)],
-                [X[i,1] for i in xrange(N)],
-                'y.',
+                'k-',
+                [X[i,0] for i in xrange(N/2)],
+                [X[i,1] for i in xrange(N/2)],
+                'rx',
+                [X[i,0] for i in xrange(N/2+1,N)],
+                [X[i,1] for i in xrange(N/2+1,N)],
+                'b+',
                 c1t[0],
                 c1t[1],
-                'k^',
+                'y^',
                 c2t[0],
                 c2t[1],
-                'k^')
+                'y^')
 
         print c1t, c2t
         plot.show()
