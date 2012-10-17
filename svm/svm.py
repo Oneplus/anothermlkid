3da293332c80e120cda06e8ae22f4f66f8f610f4
Oneplus <oneplus.lau@gmail.com>
2012-10-17 13:24:11 +0800

    revise svm.py

--- svm/svm.py
+++ svm/svm.py
@@ -3,7 +3,6 @@
 import sys
 import numpy as np
 
-
 def main():
     try:
         fp=open("data/heart_scale.dat", "r")
@@ -51,28 +50,87 @@ def main():
     alpha = np.array([0.] * N)
 
     num_changed = 0
-    examine_all = 1
+    examine_all = True
 
-    def evaluate(i):
-        pass
+    kernel = None
 
-    def examine_example(i):
-        assert (i >= 0 and i < N)
-        y = Y[i]
-        alphai = alpha[i]
+    if opt.kernel   == "LINEAR":
+        kernel = lambda(u, v): np.vdot(u, v)
+    elif opt.kerenl == "RBF":
+        kernel = lambda(u,v): math.exp(-np.vdot(u-v,u-v)/sigma)
 
-        def getE(i):
-            pass
+    def getE(i):
+        return np.vdot(alpha, X[i])-Y[i]
 
-        ei = getE(i)
+    b=0.
+    C=1.
 
+    max_passes = 10
+    max_iter   = 10000
+    passes = 0
+    it = 0
+    while passes < max_passes and it < max_iter:
+        changed = 0
 
-    while num_changed > 0 or examine_all:
-        num_changed = 0
+        for i in xrange(N):
+            Ei = getE(i)
 
-        if examine_all:
-            for i in xrange(N):
-                num_changed += examine_example(i)
+            # if this data point violate the KKT condition
+            if ((Y[i]*Ei<-tol and alpha[i]<C) or
+                    (Y[i]*Ei>tol and alpha[i]>0.)):
+                j = i
+                while j == i:
+                    j = np.random.randint(0, N, size=1)
+
+                Ej = getE(j)
+
+                L=0.; H=C;
+
+                if Y[i] == Y[j]:
+                    L=max(0., alpha[i]+alpha[j]-C)
+                    H=min(C, alpha[i]+alpha[j])
+                else:
+                    L=max(0., alpha[j]-alpha[i])
+                    H=min(C, C+alpha[j]-alpha[i])
+
+                if abs(L-H)<1e-4:
+                    continue
+
+                eta=2.*kernel(X[i],X[j])-kernel(X[i],X[i])-kernel(X[j],X[j])
+                if eta >= 0.:
+                    continue
+
+                new_aj = aj - Y[j]*(Ei-Ej)/eta
+                new_aj = H if new_aj>H else new_aj
+                new_aj = L if new_aj<L else new_aj
+                if abs(new_aj-aj)<1e-4:
+                    continue
+                alpha[j] = new_aj
+
+                new_ai = ai + Y[i]*Y[j]*(aj-new_aj)
+                alpha[i] = new_ai
+
+                bi=(b-Ei-Y[i]*(new_ai-ai)*kernel(X[i],X[i])
+                        -Y[j]*(new_aj-aj)*kernel(X[i],X[j]))
+                bj=(b-Ej-Y[j]*(new_ai-ai)*kernel(X[i],X[j])
+                        -Y[j]*(new_aj-aj)*kernel(X[j],X[j]))
+
+                b=.5*(bi+bj)
+
+                if new_ai>.0 and new_ai<C:
+                    b=bi
+                if new_aj>.0 and new_aj<C:
+                    b=bj
+
+                chanaged += 1
+
+        it += 1
+
+        if changed == 0:
+            passes += 1
+        else:
+            passes = 0
+
 
 
 if __name__=="__main__":
